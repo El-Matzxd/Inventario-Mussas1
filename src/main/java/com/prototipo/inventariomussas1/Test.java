e384dbc961375f74c7fbda6ee22ffd0f30019520
Max GM <mucinomax03@gmail.com>
2024-12-07 14:14:56 -0600

    Primer commit para crear el repositorio, solo contiene le codigo para probar la conexion a BD

---
+++ src/main/java/com/prototipo/inventariomussas1/Test.java
@@ -0,0 +1,16 @@
+package com.prototipo.inventariomussas1;
+
+import java.sql.Connection;
+
+public class Test {
+    public static void main(String[] args) {
+        ConexionBD conexionBD = new ConexionBD();
+        Connection conexion = conexionBD.conectar();
+
+        if (conexion != null) {
+            System.out.println("Conexion exitosa a BD");
+        } else {
+            System.out.println("Error al conectar a BD");
+        }
+    }
+}
